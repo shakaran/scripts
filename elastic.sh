eb7ff1fc377905c98c7c277114dbbc4e6c57c0ef
Ángel Guzmán Maeso <angel@guzmanmaeso.com>
2024-08-11 21:16:21 +0200

    feat: add elastic easy setup script on debian

---
+++ elastic.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#    elastic is a bash script for installing Elasticsearch 8.x on Debian-based systems.
+#    @see https://www.elastic.co/guide/en/elasticsearch/reference/current/deb.html
+
+#    Copyright (C) 2024 by Ángel Guzmán Maeso, shakaran at gmail dot com
+
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+if [ `/usr/bin/id -u` -ne 0 ]; then
+    echo 'Please, run this script as root'
+    exit 1
+fi
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+sudo apt-get install apt-transport-https
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
